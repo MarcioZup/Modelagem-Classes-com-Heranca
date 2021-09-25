47b9b24cf0ce348b66cf6a8af274ed9e76d54376
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-09-24 21:32:40 -0300

    criação da classe Main e testados métodos da classe Colaborador

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Instanciando objeto fornecedor1
         Fornecedor fornecedor1 = new Fornecedor("Armazém Paraíba", "Campina Grande/PB", "83-2244-5566", 50000, 0);
 
+        //Testando os métodos para classe Fornecedor
         System.out.println("O valor atual do crédito é R$ " + fornecedor1.getValorCredito());
         fornecedor1.setValorDivida(51000);
         System.out.println("O valor atual do crédito é R$ " + fornecedor1.obterSaldo());
@@ -10,6 +12,15 @@ public class Main {
         System.out.println("O valor atual do crédito é R$ " + fornecedor1.obterSaldo());
         fornecedor1.setValorCredito((fornecedor1.getValorCredito() + 70000));
         System.out.println("O valor atual do crédito é R$ " + fornecedor1.obterSaldo());
+
+        //Instanciando objeto colaborador1
+        Colaborador colaborador1 = new Colaborador("João", "Avenida 1", "11-95874-4522", 101, 3500, 0.275);
+
+        //Testando métodos para classe Colaborador
+        System.out.println("O salário bruto do colaborador é R$ " + colaborador1.getSalarioBase());
+        colaborador1.calculaSalario();
+        System.out.println("O salário líquido do colaborador é R$ " + colaborador1.calculaSalario());
+
 
     }
 }
